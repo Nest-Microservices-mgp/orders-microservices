6bec9b963434d5991838df73e7e633daba47c99f
mgarces <manugp1994@gmail.com>
2025-08-01 13:23:57 +0200

    Video54

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { envs } from './config';
 
 async function bootstrap() {
   const logger = new Logger('Orders-Main');
 
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: envs.port,
+    },
+  });
+  await app.listen();
 
   logger.log(`Microservice is running on: http://localhost:${envs.port}`);
 }
